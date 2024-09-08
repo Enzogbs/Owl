efccc45ebd0d7fab5767687744a35ee89b208935
Enzo Soares <44247248+Enzogbs@users.noreply.github.com>
2024-09-08 15:35:16 -0300

    Update apis.py

--- owl/apis.py
+++ owl/apis.py
@@ -1,1 +1,58 @@
+import aiohttp
+import asyncio
 
+def bing_fetch(key: str, search_term: str, count: str):
+    SEARCH_URL = "https://api.bing.microsoft.com/v7.0/images/search"
+
+    headers = {"Ocp-Apim-Subscription-Key" : key}
+
+    params  = {
+        "q": search_term, 
+        "license": "public", 
+        "imageType": "photo",
+        "count": count
+        }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(SEARCH_URL, params=params) as response:
+            response.raise_for_status()
+            response_data = await response.json()
+            image_urls = [img["thumbnailUrl"] for img in response_data["value"]]
+
+    return image_urls
+
+def google_fetch(key: str, engine_id: str, search_term: str, count: int):   
+    SEARCH_URL = "https://www.googleapis.com/customsearch/v1"
+
+    params = {
+        "q": search_term,
+        "key": key,
+        "cx": engine_id,
+        "searchType": "image",
+        "filter": 1
+        }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(SEARCH_URL, params=params) as response:
+            response.raise_for_status()
+            response_data = await response.json()
+            image_urls = [img["link"] for img in response_data["items"]]
+    
+    return image_urls
+  
+async def hugging_face_fetch(search_term: str, count: int):
+    SEARCH_URL = "https://huggingpics-api-server.fly.dev/images/search"
+    params = {
+        "q": search_term,
+        "license": "public",
+        "imageType": "photo",
+        "count": count
+    }
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.get(SEARCH_URL, params=params) as response:
+            response.raise_for_status()
+            response_data = await response.json()
+            image_urls = [img["thumbnailUrl"] for img in response_data["value"]]
+    
+    return image_urls
