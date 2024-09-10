20c8cc6e1448aa52bd03b279be8231730cc4fd95
Enzo Soares <44247248+Enzogbs@users.noreply.github.com>
2024-09-10 17:41:24 -0300

    Update utils.py

--- owl/utils.py
+++ owl/utils.py
@@ -1,19 +1,23 @@
-import aiohttp
-import asyncio
+import requests
 from PIL import Image
 from io import BytesIO
 
-async def download_image(session, url, path):
-    failed = 0
-    try:
-        async with session.get(url) as response:
-            response.raise_for_status()
-            img_data = await response.read()
-            img = Image.open(BytesIO(img_data))
-            img_format = img.format.lower()
-            img.save(path)
-            print(f"Downloaded: {path}")
-    
-    except Exception as e:
-        print(f"Failed to download {url}: {e}")
-        failed += 1
+def download_image(url, path, resize, verbose=True):
+	try:
+		response = requests.get(url)
+		response.raise_for_status()
+		img_data = response.content
+		img = Image.open(BytesIO(img_data))
+
+		if resize is not None:
+			img = img.resize(resize)
+
+		img.save(path)
+		
+		if verbose:
+			print(f"Downloaded: {path}")
+		return True
+
+	except Exception as e:
+		print(f"Failed to download {url}: {e}")
+		return False
