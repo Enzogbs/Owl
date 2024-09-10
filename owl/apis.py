7d4c5b885ec1b90bc3b4173c98719ba022101c15
Enzo Soares <44247248+Enzogbs@users.noreply.github.com>
2024-09-10 17:42:24 -0300

    Update apis.py

--- owl/apis.py
+++ owl/apis.py
@@ -1,100 +1,82 @@
-import aiohttp
-import asyncio
-import sys
+import requests
 
+def bing_fetch(key: str, search_term: str, count: str):
+	SEARCH_URL = "https://api.bing.microsoft.com/v7.0/images/search"
 
-async def bing_fetch(key: str, search_term: str, count: str):
-    SEARCH_URL = "https://api.bing.microsoft.com/v7.0/images/search"
+	headers = {"Ocp-Apim-Subscription-Key" : key}
 
-    headers = {"Ocp-Apim-Subscription-Key" : key}
+	params  = {
+		"q": search_term, 
+		"license": "public", 
+		"imageType": "photo",
+		"count": count
+		}
 
-    params  = {
-        "q": search_term, 
-        "license": "public", 
-        "imageType": "photo",
-        "count": count
-        }
+	try:
+		response = requests.get(search_url, headers=headers, params=params)
+		response.raise_for_status()
+		response_data = response.json()
+		image_urls = [img["thumbnailUrl"] for img in response_data["value"][:16]]
 
-    async with aiohttp.ClientSession() as session:
-        try:
-            async with session.get(SEARCH_URL, params=params) as response:
-                response.raise_for_status()
-                response_data = await response.json()
-                image_urls = [img["thumbnailUrl"] for img in response_data["value"]]
-        
-        except aiohttp.ClientError as e:
-            print(f"HTTP request failed: {e}")
-            sys.exit()
-        
-        except KeyError as e:
-            print(f"Unexpected response format: {e}")
-            sys.exit()
+	except Exception as e:
+		print("Failed to fetch images")
 
-    return image_urls
+	return image_urls
 
-async def google_fetch(key: str, engine_id: str, search_term: str, count: int):
-    SEARCH_URL = "https://www.googleapis.com/customsearch/v1"
-    
-    params = {
-        "q": search_term,
-        "key": key,
-        "cx": engine_id,
-        "searchType": "image",
-        "filter": 1
-        }
-    
-    image_urls = []
-    start = 1
+def google_fetch(key: str, engine_id: str, search_term: str, count: int):	
+	SEARCH_URL = "https://www.googleapis.com/customsearch/v1"
 
-    async with aiohttp.ClientSession() as session:
-        while len(image_urls) < count:
-            params['start'] = start
-            try:
-                async with session.get(SEARCH_URL, params=params) as response:
-                    response.raise_for_status()
-                    response_data = await response.json()
-                    items = response_data["items"]
+	params = {
+		"q": search_term,
+		"key": key,
+		"cx": engine_id,
+		"searchType": "image",
+		"filter": 1
+		}
+	
+	image_urls = []
+	start = 1
 
-                    if not items:
-                        break
-                        
-                    image_urls.extend([img["link"] for img in items])
-                    
-                    start += 10
-            
-            except aiohttp.ClientError as e:
-                print(f"HTTP request failed: {e}")
-                sys.exit()
-            
-            except KeyError as e:
-                print(f"Unexpected response format: {e}")
-                sys.exit()
+	while len(image_urls) < count:
+		params["start"] = start
+		
+		try:
+			response = requests.get(SEARCH_URL, params=params)
+			response.raise_for_status()
+			response_data = response.json()
+			items = response_data["items"]
 
-    return image_urls
+			if not items:
+				break
 
-async def hugging_face_fetch(search_term: str, count: int):
+			image_urls.extend([img["link"] for img in items])
+
+			start += 10
+
+		except Exception as e:
+			print("Failed to fetch images")
+			continue
+
+	return image_urls		
+	
+
+def hugging_face_fetch(search_term: str, count: int):
     SEARCH_URL = "https://huggingpics-api-server.fly.dev/images/search"
-    
+
     params = {
         "q": search_term,
         "license": "public",
         "imageType": "photo",
         "count": count
-        }
-    
-    async with aiohttp.ClientSession() as session:
-        try:
-            async with session.get(SEARCH_URL, params=params) as response:
-                response.raise_for_status()
-                response_data = await response.json()
-                image_urls = [img["thumbnailUrl"] for img in response_data["value"]]
-        
-        except aiohttp.ClientError as e:
-            print(f"HTTP request failed: {e}")
-            sys.exit()
-        
-        except KeyError as e:
-            print(f"Unexpected response format: {e}")
-            sys.exit()                
-    
+    	}
+
+    try:
+    	response = requests.get(SEARCH_URL, params=params)
+    	response.raise_for_status()
+    	response_data = response.json()
+    	image_urls = [img["thumbnailUrl"] for img in response_data["value"]]
+
+    except Exception as e:
+    	print("Failed to fetch images")
+
     return image_urls
